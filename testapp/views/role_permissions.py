8d8b72fb251401b3fcb4f0dd2bad387c0c44960b
salmarazzaq257 <166105479+salmarazzaq257@users.noreply.github.com>
2024-12-01 21:53:57 +0500

    review the user_management

--- testapp/views/role_permissions.py
+++ testapp/views/role_permissions.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdmi
 from rest_framework import status
 from testapp.models.role_permission import RolePermission
 from testapp.serializers.role_permission_serializers import RolePermissionSerializer
